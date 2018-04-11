22d4c3e7a164486031f2e5a8fb28d75fac929a39
alok.bichhwe <alok.bichhwe@systematixindia.com>
2018-04-11 18:25:48 +0530

    update response

--- server/api/activeMember.js
+++ server/api/activeMember.js
@@ -7,15 +7,33 @@ exports.activeMember = function (ds,MobileNo,OTP,cb) {
     "\t\t@OTP = '"+OTP+"'\n" +
     "\n" +
     "SELECT\t'Return Value' = @return_value";
-    //console.log(sql); return false;
   ds.connector.query(sql, function (err, data) {
     if (err) {
       cb(null,{status:"0",message:"fail"});
-
     }
     else
     {
-      cb(null, data[0]);
+    	if(data[0] != undefined){
+    		if(data[0].CustomerNo != undefined && data[0].CustomerNo === 0){
+    			var error = new Error("");
+				error.status = 401;
+				return cb(error);
+
+    			//cb(401,{status:"0",message:"OTP does not match"});
+    		}else if(data[0].CustomerNo != undefined && data[0].CustomerNo === 1){
+    			var error = new Error("");
+				error.status = 401;
+				return cb(error);
+
+    			//cb(401,{status:"1",message:"user not found"});
+    		}else if(data[0].CustomerNo != undefined){
+    			cb(null, data[0]);
+    		}else{
+    			cb(null,{status:"0",message:"fail"});
+    		}
+    	}else{
+    		cb(null,{status:"0",message:"fail"});
+    	}
 
     }
   });
